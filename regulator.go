056592a21590b380fabe9da8b0c73bffd2e02124
Frankie O'Rourke <frankie@orourkedd.com>
2018-08-07 14:39:08 -0700

    return errors without unnecessay operations

--- regulator.go
+++ regulator.go
@@ -27,7 +27,12 @@ func NewRegulator(concurrency int) *Regulator {
 
 func (regulator *Regulator) Execute(job func() error) {
 	index := atomic.AddInt32(&regulator.jobIndex, 1)
+	// if error exists, return
+	if regulator.err != nil {
+		return
+	}
 	regulator.sem <- true
+	// if error occurred while waiting for channel to unblock, return
 	if regulator.err != nil {
 		<-regulator.sem
 		return
